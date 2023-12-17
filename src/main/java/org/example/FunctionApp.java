d256c311f958d015b1cabf50e283a2959be8f6f4
ShalaevPavel <lohgewt123@yandex.by>
2023-12-17 13:11:43 +0300

    added I18n

--- src/main/java/org/example/FunctionApp.java
+++ src/main/java/org/example/FunctionApp.java
@@ -2,42 +2,36 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class FunctionApp {
 
-    private FunctionLoader functionLoader;
     private JFrame frame;
     private JComboBox<String> functionComboBox;
     private JTextField argumentsField;
     private JLabel resultLabel;
+    private FunctionLoader functionLoader;
+    private ResourceBundle messages;
 
     public FunctionApp() {
+        functionLoader = new FunctionLoader("src/main/resources/calc.properties");
+        messages = ResourceBundle.getBundle("messages", Locale.getDefault());
         initializeUI();
     }
 
     private void initializeUI() {
-        frame = new JFrame("Function Calculator");
+        frame = new JFrame(messages.getString("title"));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-        functionComboBox = new JComboBox<>();
-        functionComboBox.addItem("function1");
-        functionComboBox.addItem("function2");
-        // Добавьте здесь другие функции
-
+        functionComboBox = new JComboBox<>(new String[]{"function1", "function2"});
         argumentsField = new JTextField();
-        JButton executeButton = new JButton("Execute");
+        JButton executeButton = new JButton(messages.getString("execute"));
+        resultLabel = new JLabel(messages.getString("result"));
 
-        resultLabel = new JLabel("Result: ");
-
-        executeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                executeFunction();
-            }
-        });
+        executeButton.addActionListener(this::executeFunction);
 
         frame.add(functionComboBox);
         frame.add(argumentsField);
@@ -47,13 +41,19 @@ public class FunctionApp {
         frame.setVisible(true);
     }
 
-    private void executeFunction() {
+    private void executeFunction(ActionEvent e) {
         String functionName = (String) functionComboBox.getSelectedItem();
         String argument = argumentsField.getText();
-
+        try {
+            Object result = functionLoader.invokeFunction(functionName, Double.parseDouble(argument));
+            resultLabel.setText(messages.getString("result") + ": " + result);
+        } catch (Exception ex) {
+            resultLabel.setText(messages.getString("error") + ": " + ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        new FunctionApp();
+        SwingUtilities.invokeLater(FunctionApp::new);
     }
 }

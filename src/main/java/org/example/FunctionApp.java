28fc96b235407696df23ce3852a891a08ff3c007
ShalaevPavel <lohgewt123@yandex.by>
2023-12-17 13:47:57 +0300

    added locale

--- src/main/java/org/example/FunctionApp.java
+++ src/main/java/org/example/FunctionApp.java
@@ -13,13 +13,46 @@ public class FunctionApp {
     private JLabel resultLabel;
     private FunctionLoader functionLoader;
     private ResourceBundle messages;
+    private JButton languageButton;
+    private JButton executeButton;
+    private Locale currentLocale = Locale.ENGLISH;
 
     public FunctionApp() {
         functionLoader = new FunctionLoader("src/main/resources/calc.properties");
         messages = ResourceBundle.getBundle("messages", Locale.getDefault());
         initializeUI();
+        initializeLanguageButton();
+
     }
 
+    private void initializeLanguageButton() {
+        languageButton = new JButton("Сambiar idioma");
+        languageButton.addActionListener(this::toggleLanguage);
+
+        frame.add(languageButton);
+    }
+
+    private void toggleLanguage(ActionEvent e) {
+        if (currentLocale.equals(Locale.ENGLISH)) {
+            currentLocale = new Locale("ru", "RU");
+        } else {
+            currentLocale = Locale.ENGLISH;
+        }
+        updateLanguage();
+    }
+
+    private void updateLanguage() {
+        // Загрузка ресурсов для новой локали
+        messages = ResourceBundle.getBundle("messages", currentLocale);
+
+        // Обновление текстов интерфейса
+        frame.setTitle(messages.getString("title"));
+        executeButton.setText(messages.getString("execute"));
+        resultLabel.setText(messages.getString("result"));
+        languageButton.setText(messages.getString("change_language"));
+
+        // Обновите здесь любые другие текстовые компоненты, которые требуют локализации
+    }
     private void initializeUI() {
         frame = new JFrame(messages.getString("title"));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,7 +61,8 @@ public class FunctionApp {
 
         functionComboBox = new JComboBox<>(new String[]{"function1", "function2"});
         argumentsField = new JTextField();
-        JButton executeButton = new JButton(messages.getString("execute"));
+        executeButton = new JButton(messages.getString("execute"));
+
         resultLabel = new JLabel(messages.getString("result"));
 
         executeButton.addActionListener(this::executeFunction);
@@ -46,9 +80,12 @@ public class FunctionApp {
         String argument = argumentsField.getText();
         try {
             Object result;
-            if (argument.isEmpty() && !functionName.equals("function1")) { // Предполагаем, что function1 не требует аргументов
+            // Проверяем, пустая ли строка
+            if (argument.isEmpty()) {
+                // Вызываем функцию без аргументов
                 result = functionLoader.invokeFunction(functionName);
             } else {
+                // Пытаемся преобразовать строку в double и вызвать функцию с этим аргументом
                 result = functionLoader.invokeFunction(functionName, Double.parseDouble(argument));
             }
             resultLabel.setText(messages.getString("result") + ": " + result);
@@ -59,6 +96,7 @@ public class FunctionApp {
             ex.printStackTrace();
         }
     }
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(FunctionApp::new);
